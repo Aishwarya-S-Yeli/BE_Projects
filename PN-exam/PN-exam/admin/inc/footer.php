0066eb70b15324cea5c6ed217fabd83e6e403f62
Aishwarya-S-Yeli <86348860+Aishwarya-S-Yeli@users.noreply.github.com>
2021-09-27 20:45:33 +0530

    Add files via upload

---
+++ PN-exam/PN-exam/admin/inc/footer.php
@@ -0,0 +1,7 @@
+ </section>
+<section class="footeroption">
+		<h2><?php echo "GM INSTITUTE OF TECHNOLOGY"; ?></h2>
+	</section>
+</div>
+</body>
+</html>
---
+++ PN-exam/PN-exam/inc/footer.php
@@ -0,0 +1,7 @@
+ </section>
+<section class="footeroption">
+		<h2><?php echo "WELCOME"; ?></h2>
+	</section>
+</div>
+</body>
+</html>
